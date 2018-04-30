380f053ec9e73597a743b38a3ece1ad3f04a5827
e_daniel <daniel.j.ernst1@gmail.com>
2018-04-30 10:45:01 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/JonoAugustine/WeebBot""
    This reverts commit b48f28f8099ad8a75c796a7d709e765c46a4e043.

---
+++ src/com/ampro/main/bot/commands/Command.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+package com.ampro.main.bot.commands;
+
+/**
+ * A {@code Command}
+ * ? Not sure if this is actually the way-to-go.
+ * ? The way i've seen it implemented, this is basically just another listner
+ */
+public class Command {
+
+}
