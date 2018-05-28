dd53c7ac1ced2a9f1201ec7f1a97e20ec4487db5
JonoAugustine <jaugust8@jhu.edu>
2018-05-28 01:51:20 -0400

    OHC disallow duplicates||GlobalWeebot USER_PASSIVES hashmap<userID,List>

--- src/main/java/com/ampro/weebot/commands/miscellaneous/OutHouseCommand.java
+++ src/main/java/com/ampro/weebot/commands/miscellaneous/OutHouseCommand.java
@@ -3,17 +3,21 @@ package com.ampro.weebot.commands.miscellaneous;
 import com.ampro.weebot.Launcher;
 import com.ampro.weebot.commands.Command;
 import com.ampro.weebot.commands.IPassive;
+import com.ampro.weebot.entities.bot.GlobalWeebot;
 import com.ampro.weebot.entities.bot.Weebot;
 import com.ampro.weebot.listener.events.BetterMessageEvent;
+import com.sun.org.apache.bcel.internal.generic.LADD;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.entities.User;
 
+import javax.swing.*;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Have the bot respond to your metions while you're AFK but shown as online. <br>
@@ -21,9 +25,7 @@ import java.util.Arrays;
  */
 public class OutHouseCommand extends Command {
 
-    /**
-     * An instantiable representation of a User's OutHouse.
-     */
+    /** An instantiable representation of a User's OutHouse. */
     public static final class OutHouse implements IPassive {
 
         final User user;
@@ -106,6 +108,21 @@ public class OutHouseCommand extends Command {
     protected void execute(Weebot bot, BetterMessageEvent event) {
         //ohc [hours] [message here]
         StringBuilder sb = new StringBuilder();
+        List<IPassive> pas = GlobalWeebot.getUserPassives(event.getAuthor());
+
+        //Check if the user already has a OHC running
+        if (pas != null) {
+            for (IPassive p : pas)
+                if (p instanceof OutHouse) {
+                    sb.append("You're already in the outhouse");
+                    if(((OutHouse) p).message != null)
+                        sb.append(" " + ((OutHouse) p).message);
+                    sb.append(".");
+                    event.reply(sb.toString());
+                    return;
+                }
+        }
+
         String[] args = this.cleanArgs(bot, event.getArgs());
         long hours;
         int messageIndex;
@@ -127,9 +144,12 @@ public class OutHouseCommand extends Command {
         } else {
             oh = new OutHouse(event.getAuthor(), hours, m);
         }
-        Launcher.GLOBAL_WEEBOT.getPassives().add(oh);
 
-        event.reply("I will hold down the fort while you're away! :guardsman: ");
+
+        if (GlobalWeebot.addUserPassive(event.getAuthor(), oh))
+            event.reply("I will hold down the fort while you're away! :guardsman: ");
+        else
+            event.reply("Sorry, something went wrong :cry:. Please try again later.");
 
     }
 
--- src/main/java/com/ampro/weebot/entities/bot/GlobalWeebot.java
+++ src/main/java/com/ampro/weebot/entities/bot/GlobalWeebot.java
@@ -3,32 +3,34 @@ package com.ampro.weebot.entities.bot;
 import com.ampro.weebot.commands.IPassive;
 import com.ampro.weebot.listener.events.BetterEvent;
 import com.ampro.weebot.listener.events.BetterMessageEvent;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.lang.management.LockInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class GlobalWeebot extends Weebot implements EventListener {
+
+    /** A Map of {@link IPassive} objects mapped to user IDs */
+    private static final ConcurrentHashMap<Long, List<IPassive>> USER_PASSIVES
+            = new ConcurrentHashMap<>();
 
     /**
      * Create a Weebot with no guild or call sign.
      * <b>This is only called for the private-chat instance Weebot created
      * when a new Database is created.</b>
      */
-    public GlobalWeebot() {
-        super();
-    }
+    public GlobalWeebot() { super(); }
 
     /**
-     * Handles any {@link Event Event}.
-     *
-     * <p>To get specific events with Methods like {@code onMessageReceived
-     * (MessageReceivedEvent
-     * event)}
-     * take a look at: {@link ListenerAdapter ListenerAdapter}
-     *
-     * @param event
-     *         The Event to handle.
+     * Distribute each event to all the Global Weebot's {@link IPassive} objects.
+     * @param event  The Event to handle.
      */
     @Override
     public void onEvent(Event event) {
@@ -49,4 +51,53 @@ public class GlobalWeebot extends Weebot implements EventListener {
         }
     }
 
+    /** @return */
+    public static final synchronized Map<Long, List<IPassive>> getUserPassives() {
+        return USER_PASSIVES;
+    }
+
+    /**
+     * @param user The user
+     * @return The List of {@link com.ampro.weebot.commands.IPassive IPassives} mapped
+     * to the given user. Null if there are no passives mapped to the user.
+     */
+    public static final synchronized List<IPassive> getUserPassives(User user) {
+        return USER_PASSIVES.get(user.getIdLong());
+    }
+
+    /**
+     * Add an {@link IPassive} to the the list mapped to the {@link User} given.<br>
+     * If there is no mapping to the {@link User user} or it is mapped to {@code null},
+     * a new {@link List} is created, mapped to the {@link User user} and the
+     * {@link IPassive} is added.
+     *
+     * @param user The user
+     * @param passive The passive to map to the User
+     * @return {@code false} if the {@link IPassive} already exists in the
+     * {@link User user's} list of {@link IPassive passives}.
+     */
+    public static final synchronized boolean addUserPassive(User user, IPassive passive) {
+        List<IPassive> pasList = USER_PASSIVES.get(user);
+        if (pasList == null) {
+            pasList = new ArrayList<>();
+            USER_PASSIVES.put(user.getIdLong(), pasList);
+        }
+        return pasList.add(passive);
+    }
+
+    /**
+     * @param passive
+     * @return {@code true} if the user's passive list contains the parameter.<br>
+     *     {@code false} if the user's passive list does not contain the parameter or
+     *     is null
+     */
+    public static final synchronized boolean userHasPassive(User user, IPassive passive) {
+        try {
+            return USER_PASSIVES.get(user.getIdLong()).contains(passive);
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
 }
+
