21e874ae7bbe00c3b6b0b6f958e6a28f64f6d74b
jaugust8 <jaugust8@jhu.edu>
2019-10-14 19:03:39 -0400

    ping frame & pastebin API Key

--- src/main/kotlin/com/ampro/weebot/bot/Credentials.kt
+++ src/main/kotlin/com/ampro/weebot/bot/Credentials.kt
@@ -9,4 +9,7 @@ internal object Credentials {
         const val weebot = "NDM3ODUxODk2MjYzMjEzMDU2.XYsEmw.CM4yg4NLbWWaFCIObzSXSOTCNXA"
         const val tobeew = "NDQ0MzIzNzMyMDEwMzAzNDg4.XYsEsg.LWDdvxIllwa6Toc6qCne1BVbTRw"
     }
+    object APIs {
+        const val pastebin = "b847d5fe743bfa471aad4bc7b317c1ac"
+    }
 }
